928422b7972362da34063593d0bd1c160888eb17
How Bizarre <howbizarre@gmail.com>
2024-02-01 10:38:11 +0200

    Add margin top to article and default pages

--- layouts/article.vue
+++ layouts/article.vue
@@ -2,7 +2,7 @@
   <main id="articles-layout" class="relative max-w-3xl px-4 mx-auto">
     <ArticleHead />
 
-    <div id="articles-page">
+    <div id="articles-page" class="mt-3">
       <slot />
     </div>
   </main>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <main id="default-layout" class="relative max-w-3xl px-4 mx-auto">
     <Header />
 
-    <div id="default-page">
+    <div id="default-page" class="mt-3">
       <slot />
     </div>
   </main>
