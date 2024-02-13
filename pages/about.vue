c34d06a5ed7e8cd92679178bc41d68be01d62c32
How Bizarre <howbizarre@gmail.com>
2024-02-13 09:53:19 +0200

    Refactor about and thoughts pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,15 @@
 <template>
-  <main>
-    <article class="rounded-2xl">
-      <ContentDoc v-slot="{ doc }">
-        <div class="mt-4">
-          <header class="p-5">
-            <h1 class="text-4xl font-bold lg:w-2/3 text-left">{{ doc.title }}</h1>
-          </header>
+  <div class="rounded-2xl">
+    <ContentDoc v-slot="{ doc }">
+      <div class="mt-4">
+        <header class="p-5">
+          <h1 class="text-4xl font-bold lg:w-2/3 text-left">{{ doc.title }}</h1>
+        </header>
 
-          <div class="p-5">
-            <ContentRenderer :value="doc" />
-          </div>
+        <div class="p-5">
+          <ContentRenderer :value="doc" />
         </div>
-      </ContentDoc>
-    </article>
-  </main>
+      </div>
+    </ContentDoc>
+  </div>
 </template>
--- pages/thoughts/[...slug].vue
+++ pages/thoughts/[...slug].vue
@@ -6,38 +6,36 @@ const { slug } = route.params;
 </script>
 
 <template>
-  <main class="rounded-2xl mb-4">
-    <article>
-      <ContentDoc :path="localePath(`${slug}`)" v-slot="{ doc }">
-        <div class="mt-4">
-          <header>
-            <div class="text-left p-5">
-              <p class="text-gray-500 text-sm !-mb-1 block">{{ (new Date(doc.date)).toLocaleDateString(locale) }}</p>
-              <h1 class="text-4xl font-bold lg:w-2/3">{{ doc.title }}</h1>
-            </div>
-          </header>
+  <article class="rounded-2xl mb-4">
+    <ContentDoc :path="localePath(`${slug}`)" v-slot="{ doc }">
+      <div class="mt-4">
+        <header>
+          <div class="text-left p-5">
+            <p class="text-gray-500 text-sm !-mb-1 block">{{ (new Date(doc.date)).toLocaleDateString(locale) }}</p>
+            <h1 class="text-4xl font-bold lg:w-2/3">{{ doc.title }}</h1>
+          </div>
+        </header>
 
-          <div class="px-5">
-            <ContentRenderer :value="doc" />
+        <div class="px-5">
+          <ContentRenderer :value="doc" />
+        </div>
+
+        <hr class="h-px my-4 bg-gray-300 border-0 dark:bg-gray-700" />
+
+        <div class="px-5 pb-5">
+          <div v-if="doc.tags">
+            {{ t("LBL_TAGS") }}:
+            <template v-for="tag in doc.tags">
+              <Tag :tag="tag" />
+            </template>
           </div>
 
-          <hr class="h-px my-4 bg-gray-300 border-0 dark:bg-gray-700" />
-
-          <div class="px-5 pb-5">
-            <div v-if="doc.tags">
-              {{ t("LBL_TAGS") }}:
-              <template v-for="tag in doc.tags">
-                <Tag :tag="tag" />
-              </template>
-            </div>
-            
-            <div v-if="doc.competence" class="mt-2">
-              {{ t("LBL_COMPETENCE") }}:
-              <Competence :competence="doc.competence" />
-            </div>
+          <div v-if="doc.competence" class="mt-2">
+            {{ t("LBL_COMPETENCE") }}:
+            <Competence :competence="doc.competence" />
           </div>
         </div>
-      </ContentDoc>
-    </article>
-  </main>
+      </div>
+    </ContentDoc>
+  </article>
 </template>
