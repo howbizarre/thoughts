4c60ec675a9fbf912da599f0062c9009a5275aeb
How Bizarre <howbizarre@gmail.com>
2024-02-18 18:15:36 +0200

    Add i18n support to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+const i18nHead = useLocaleHead();
+
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])]
+});
+</script>
+
 <template>
   <main id="default-layout" class="max-w-3xl mx-auto p-4 md:px-0">
     <Logos />
