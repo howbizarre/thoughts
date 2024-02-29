32df7bef0b2b750f235e8da781743f747b485714
How Bizarre <howbizarre@gmail.com>
2024-02-29 12:15:35 +0200

    Delete unnecessary content and refactor index.vue

--- content/bg/index.md
+++
@@ -1,6 +0,0 @@
----
-title: "How Bizarre's Thoughts"
-date: "2024-02-04T08:09:10.000Z"
-slug: ""
-navigation: false
----
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { ChevronDownIcon } from '@heroicons/vue/24/outline';
-import type { QueryBuilderParams } from '@nuxt/content/types';
 
 const localePath = useLocalePath();
 const articlesPath = localePath('/articles');
@@ -22,10 +21,6 @@ const loadMore = async () => {
   if (articles.value.length >= countArticles) haveReachedEnd.value = true;
 };
 
-const query: QueryBuilderParams = import.meta.dev
-  ? { path: articlesPath, sort: [{ date: -1 }] }
-  : { path: articlesPath, where: [{ draft: false }], sort: [{ date: -1 }] };
-
 const description = {
   "bg": "Статии, предимно за Vue, Nuxt, TailwindCSS, TypeScript, но не само. Повече за front-end и по-малко за back-end.",
   "en": "Articles mostly about Vue, Nuxt, TailwindCSS, and TypeScript, but not limited to — more on the front-end and less on the back-end."
@@ -39,46 +34,6 @@ useHead({
 
 <template>
   <div class="grid grid-cols-1 gap-10">
-    <ContentList :query="query">
-      <template #default="{ list }">
-        <div v-for="doc in list" :key="doc._path" class="excerpt-card">
-          <div class="text-xs text-gray-500 -mb-1 block">
-            {{ (new Date(doc.date)).toLocaleDateString(locale) }}
-          </div>
-
-          <div class="hN text-2xl font-bold">
-            <NuxtLink :to="localePath(`/articles/${doc.slug}`)">{{ doc.title }}</NuxtLink>
-          </div>
-
-          <div v-if="doc?.excerpt" class="my-3">
-            <ContentRendererMarkdown :value="doc.excerpt" />
-          </div>
-
-          <hr class="h-line" />
-
-          <div class="grid grid-col-1 sm:flex sm:justify-start sm:items-center gap-2 sm:gap-5">
-            <div v-if="doc.competence">
-              {{ t("LBL_COMPETENCE") }}:
-              <Competence :competence="doc.competence" />
-            </div>
-
-            <div v-if="doc.tags">
-              {{ t("LBL_TAGS") }}:
-              <template v-for="tag in doc.tags">
-                <Tag :tag="tag" />
-              </template>
-            </div>
-          </div>
-        </div>
-      </template>
-
-      <template #not-found>
-        <p>The content is not added!</p>
-      </template>
-    </ContentList>
-
-    <hr />
-
     <div v-for="article in articles" class="excerpt-card">
       <ContentRenderer :value="article">
         <div class="text-xs text-gray-500 -mb-1 block">
