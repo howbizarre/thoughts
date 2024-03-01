b99868c1773400c835d9a5750b7dcce57d523790
How Bizarre <howbizarre@gmail.com>
2024-03-01 17:14:17 +0200

    Update query limit in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ const localePath = useLocalePath();
 const { locale, t } = useI18n();
 const articlesPath = localePath('/articles');
 
-const query: QueryBuilderParams = { path: articlesPath, where: [{ draft: false }], sort: [{ date: -1 }], limit: 5 };
+const firstPage: QueryBuilderParams = { path: articlesPath, where: [{ draft: false }], sort: [{ date: -1 }], limit: 3 };
 
 const description = {
   "bg": "Статии, предимно за Vue, Nuxt, TailwindCSS, TypeScript, но не само. Повече за front-end и по-малко за back-end.",
@@ -20,32 +20,32 @@ useHead({
 
 <template>
   <div class="grid grid-cols-1 gap-10">
-    <ContentList :query="query">
+    <ContentList :query="firstPage">
       <template #default="{ list }">
-        <div v-for="doc in list" :key="doc._path" class="excerpt-card">
+        <div v-for="article in list" :key="article._path" class="excerpt-card">
           <div class="text-xs text-gray-500 -mb-1 block">
-            {{ (new Date(doc.date)).toLocaleDateString(locale) }}
+            {{ (new Date(article.date)).toLocaleDateString(locale) }}
           </div>
 
           <div class="hN text-2xl font-bold">
-            <NuxtLink :to="localePath(`/articles/${doc.slug}`)">{{ doc.title }}</NuxtLink>
+            <NuxtLink :to="localePath(`/articles/${article.slug}`)">{{ article.title }}</NuxtLink>
           </div>
 
-          <div v-if="doc?.excerpt" class="my-3">
-            <ContentRendererMarkdown :value="doc.excerpt" />
+          <div v-if="article?.excerpt" class="my-3">
+            <ContentRendererMarkdown :value="article.excerpt" />
           </div>
 
           <hr class="h-line" />
 
           <div class="grid grid-col-1 sm:flex sm:justify-start sm:items-center gap-2 sm:gap-5">
-            <div v-if="doc.competence">
+            <div v-if="article.competence">
               {{ t("LBL_COMPETENCE") }}:
-              <Competence :competence="doc.competence" />
+              <Competence :competence="article.competence" />
             </div>
 
-            <div v-if="doc.tags">
+            <div v-if="article.tags">
               {{ t("LBL_TAGS") }}:
-              <template v-for="tag in doc.tags">
+              <template v-for="tag in article.tags">
                 <Tag :tag="tag" />
               </template>
             </div>
