faa7a096bcaeb1b440cf16d769c2bd26e0f9cd8b
How Bizarre <howbizarre@gmail.com>
2024-03-06 09:47:01 +0200

    Fix spacing in Competence and Tag components

--- components/Competence.vue
+++ components/Competence.vue
@@ -11,7 +11,7 @@ const localePath = useLocalePath();
 <template>
   <NuxtLink :to="localePath(`/competence/${competence}`)"
             :class="active ? 'bg-white text-black dark:bg-black dark:text-white' : 'bg-gray-500 !text-white'"
-            class="py-1 px-2 mx-1 rounded-xl text-xs inline-block">
+            class="py-1 px-2 m-1 rounded-xl text-xs inline-block">
     {{ t((`COMPETENCE_${competence}`).toUpperCase()) }}
   </NuxtLink>
 </template>
