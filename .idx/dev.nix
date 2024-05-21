26c6623d8fc4d212ab936ded6637aa9b0e543642
howbizarre <howbizarre@gmail.com>
2024-05-21 09:25:00 +0000

    UPD: Some configuration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -24,18 +24,18 @@
     # Enable previews
     previews = {
       enable = true;
-      # previews = {
-      #   web = {
-      #     # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-      #     # and show it in IDX's web preview panel
-      #     command = ["npm" "run" "dev"];
-      #     manager = "web";
-      #     env = {
-      #       # Environment variables to set for your server
-      #       PORT = "$PORT";
-      #     };
-      #   };
-      # };
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and show it in IDX's web preview panel
+          command = ["npm" "run" "dev"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
+      };
     };
 
     # Workspace lifecycle hooks
