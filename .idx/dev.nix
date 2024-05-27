71ee3790322bdc898b1a28bdb5cb60bea3c80cfd
howbizarre <howbizarre@gmail.com>
2024-05-27 10:23:58 +0000

    ADD: idx to scripts

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -28,7 +28,7 @@
         web = {
           # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
           # and show it in IDX's web preview panel
-          command = ["npm" "run" "dev"];
+          command = ["npm" "run" "idx"];
           manager = "web";
           env = {
             # Environment variables to set for your server
