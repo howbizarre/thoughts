40e1cd815245a531621b6054299fa7be0e486ff5
How Bizarre <howbizarre@gmail.com>
2024-09-30 17:43:52 +0300

    feat: Add placeholder translation for search input and update localization files

--- pages/search.vue
+++ pages/search.vue
@@ -14,7 +14,7 @@ const filteredResults = computed(() => results.value.filter(res => res.id.starts
 
 <template>
   <main class="grid grid-cols-1 gap-10">
-    <input v-model="search" placeholder="Type something" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+    <input v-model="search" :placeholder="t('LBL_TYPE_SOMETHING')" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 
     <template v-if="filteredResults.length">
       <div v-for="result in filteredResults" :key="result.id" class="excerpt-card">
