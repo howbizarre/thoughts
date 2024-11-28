dae675dcffffe34cca542470d6fc79f0dff0dfe4
How Bizarre <howbizarre@gmail.com>
2024-11-28 13:53:11 +0200

    feat: Update search component to conditionally render content based on filtered results

--- pages/search.vue
+++ pages/search.vue
@@ -32,7 +32,7 @@ useHead({
 
         <input v-model="search" :placeholder="t('LBL_TYPE_SOMETHING')" class="border w-[97.5%] mx-auto text-sm rounded-lg block p-2.5 bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 
-        <div class="p-5">
+        <div v-if="!filteredResults.length" class="p-5">
           <ContentRenderer :value="doc" class="text-sm prose dark:prose-invert max-w-full prose-blockquote:border-gray-500/50" />
         </div>
       </div>
