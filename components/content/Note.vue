c22d4988cc2874fe612d7e7e758aeeb8f09250fc
How Bizarre <howbizarre@gmail.com>
2025-01-20 15:40:01 +0200

    feat: Simplify ContentSlot usage in Note component by removing unnecessary prop

--- components/content/Note.vue
+++ components/content/Note.vue
@@ -5,6 +5,6 @@ import { InformationCircleIcon } from '@heroicons/vue/24/solid';
 <template>
   <div class="p-3 text-sm leading-relaxed bg-blue-200/50 border border-blue-500/25 text-black dark:bg-blue-800/50 dark:text-white rounded-lg relative">
     <InformationCircleIcon class="size-6 inline-block text-blue-500 absolute bottom-2 right-2" />
-    <ContentSlot :use="$slots.default" unwrap="p" />
+    <ContentSlot unwrap="p" />
   </div>
 </template>
